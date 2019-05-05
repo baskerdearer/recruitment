7414515a217ee55e8f6225dfea1f1a54ef231e23
baskar <baskar@192.168.0.133>
2019-05-05 18:34:58 +0530

    Swagger

--- recruitment/src/main/java/com/heavenhr/recruitment/configuration/Swagger2UiConfiguration.java
+++ recruitment/src/main/java/com/heavenhr/recruitment/configuration/Swagger2UiConfiguration.java
@@ -18,22 +18,14 @@ public class Swagger2UiConfiguration extends WebMvcConfigurerAdapter
 {
     @Bean
     public Docket api() {
-        // @formatter:off
-        //Register the controllers to swagger
-        //Also it is configuring the Swagger Docket
         return new Docket(DocumentationType.SWAGGER_2).select()
-                // .apis(RequestHandlerSelectors.any())
                 .apis(Predicates.not(RequestHandlerSelectors.basePackage("org.springframework.boot")))
-                // .paths(PathSelectors.any())
-                // .paths(PathSelectors.ant("/swagger2-demo"))
                 .build();
-        // @formatter:on
     }
  
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry)
     {
-        //enabling swagger-ui part for visual documentation
         registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
