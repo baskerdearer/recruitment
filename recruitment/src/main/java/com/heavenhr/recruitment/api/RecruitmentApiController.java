77d6598646008b696feb0a77d4374760438974c2
baskar <baskar@192.168.0.133>
2019-05-05 22:49:25 +0530

    value

--- recruitment/src/main/java/com/heavenhr/recruitment/api/RecruitmentApiController.java
+++ recruitment/src/main/java/com/heavenhr/recruitment/api/RecruitmentApiController.java
@@ -6,6 +6,7 @@ import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
---
+++ recruitment/src/main/resources/data.sql
@@ -0,0 +1,14 @@
+INSERT INTO OFFER (JOB_TITLE, START_DATE) VALUES
+  ('Senior Java Developer', '2018-05-01 06:43:19.77');
+  
+INSERT INTO OFFER (JOB_TITLE, START_DATE) VALUES
+  ('Backend Java Developer', '2019-04-03 06:43:19.77');
+
+INSERT INTO OFFER (JOB_TITLE, START_DATE) VALUES
+  ('Php Developer', '2016-03-05 06:43:19.77');
+ 
+INSERT INTO OFFER_APPLICATION (EMAIL_ID, OFFER_ID, RESUME_TEXT, STATUS) 
+    values ('abc@gmail.com', 1, 'Energetic full stack software engineer with 7+ years of experience in Agile SDLC and involved in developing robust code for high-volume businesses.', 'APPLIED');
+    
+INSERT INTO OFFER_APPLICATION (EMAIL_ID, OFFER_ID, RESUME_TEXT, STATUS) 
+    values ('efc@gmail.com', 1, 'Energetic full stack software engineer with 7+ years of experience in Agile SDLC and involved in developing robust code for high-volume businesses.', 'INVITED');
