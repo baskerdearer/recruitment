a2656ef39cbceca30fea2b11f004970fec2f7d46
baskar <baskar@192.168.0.133>
2019-05-05 13:56:21 +0530

    repository

--- recruitment/src/main/java/com/heavenhr/recruitment/offerapplication/OfferApplicationRepository.java
+++ recruitment/src/main/java/com/heavenhr/recruitment/offerapplication/OfferApplicationRepository.java
@@ -1,7 +1,9 @@
 package com.heavenhr.recruitment.offerapplication;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OfferApplicationRepository extends JpaRepository<OfferApplication, Long> {
 
 }
